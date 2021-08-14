551c9dd2775fb11c473576b966e0c2555c529e79
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-14 23:32:27 +0530

    Discriminator updated for last layer

--- model/discriminator.py
+++ model/discriminator.py
@@ -4,17 +4,26 @@ import torch.nn as nn
 
 class ConvBlock(nn.Module):
 
-    def __init__(self, chn_dim, hid_dim, kernel,stride):
+    def __init__(self, chn_dim, hid_dim, kernel,stride,final_layer=False):
         super(ConvBlock, self).__init__()
-        self.conv = nn.Conv2d(chn_dim, hid_dim, kernel,stride)
-        self.bn = nn.BatchNorm2d(hid_dim)
-        self.leakyrelu = nn.LeakyReLU(0.2)
+
+        self.final_layer = final_layer
+        if not self.final_layer:
+            self.conv = nn.Conv2d(chn_dim, hid_dim, kernel,stride)
+            self.bn = nn.BatchNorm2d(hid_dim)
+            self.leakyrelu = nn.LeakyReLU(0.2)
+        else:
+            self.conv = nn.Conv2d(chn_dim, hid_dim, kernel, stride)
+
 
     def forward(self, img):
-        img = self.conv(img)
-        img = self.bn(img)
-        img = self.leakyrelu(img)
-        return img
+        if not self.final_layer:
+            img = self.conv(img)
+            img = self.bn(img)
+            img = self.leakyrelu(img)
+            return img
+        else:
+            return self.conv(img)
 
 
 class Discriminator(nn.Module):
@@ -26,7 +35,7 @@ class Discriminator(nn.Module):
 
         self.conv1 = ConvBlock(channel_dim + 1, hid_dim, 3,2)
         self.conv2 = ConvBlock(hid_dim, hid_dim * 2, 3,1)
-        self.conv3 = ConvBlock(hid_dim * 2, 1, 3,1)
+        self.conv3 = ConvBlock(hid_dim * 2, 1, 3,1, final_layer=True)
         self.dropout = nn.Dropout(0.25)
 
     def forward(self, img, label):
@@ -44,6 +53,7 @@ class Discriminator(nn.Module):
 if __name__=="__main__":
 
     disc = Discriminator(28,3,32,10)
+    print(disc)
     img = torch.rand(4,3,28,28)
     label = torch.rand(4,1,1,10)
     vec = disc(img,label)
