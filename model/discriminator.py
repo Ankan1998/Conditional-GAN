e78c8ca9f2d91b5294eea2e0781b9373603136c4
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-14 23:04:34 +0530

    Discriminator completed

--- model/discriminator.py
+++ model/discriminator.py
@@ -17,4 +17,33 @@ class ConvBlock(nn.Module):
         return img
 
 
+class Discriminator(nn.Module):
 
+    def __init__(self, img_dim, channel_dim, hid_dim, label_dim):
+        super(Discriminator, self).__init__()
+        self.img_dim = img_dim
+        self.label_linear = nn.Linear(label_dim, self.img_dim * self.img_dim)
+
+        self.conv1 = ConvBlock(channel_dim + 1, hid_dim, 3)
+        self.conv2 = ConvBlock(hid_dim, hid_dim * 2, 3)
+        self.conv3 = ConvBlock(hid_dim * 2, 1, 3)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, img, label):
+        label = self.label_linear(label)
+        label_reshaped = torch.reshape(label, (label.shape[0],1, self.img_dim, self.img_dim))
+        concat_inp = torch.cat([img, label_reshaped], 1)
+        inp = self.conv1(concat_inp)
+        inp = self.conv2(inp)
+        inp = self.dropout(self.conv3(inp))
+        flattend = inp.view(inp.shape[0],-1)
+
+        return flattend
+
+if __name__=="__main__":
+
+    disc = Discriminator(28,3,32,10)
+    img = torch.rand(4,3,28,28)
+    label = torch.rand(4,1,1,10)
+    vec = disc(img,label)
+    print(vec.shape)
