e0ed24acb3698b616eba94ef8a70ce6ac12592f9
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-18 23:13:03 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Conditional-GAN
-Conditional GAN
+# <p align="center">Conditional-GAN</p>
+<hr>
+
+GAN variant in which both the Generator and the Discriminator are conditioned on auxiliary data such as a class label during Training.
--- loss.py
+++ loss.py
@@ -2,25 +2,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def disc_loss(gen, disc, criterion, real, label, num_images, z_dim, device):
 
+def disc_loss(gen, disc, criterion, real, label, num_images, z_dim, device):
     fake_noise = torch.randn(num_images, z_dim, device=device)
     fake_label = F.one_hot(torch.randint(0, 10, (num_images,)), num_classes=label.shape[1])
-    fake = gen(fake_noise,fake_label)
+    fake = gen(fake_noise, fake_label)
     disc_fake_pred = disc(fake.detach())
     disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
-    disc_real_pred = disc(real,label)
+    disc_real_pred = disc(real, label)
     disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
-    disc_loss = (disc_real_loss + disc_fake_loss)/2
+    disc_loss = (disc_real_loss + disc_fake_loss) / 2
     return disc_loss
 
 
-
-def gen_loss(gen, disc, criterion, num_images, z_dim, label, device,):
-
+def gen_loss(gen, disc, criterion, num_images, z_dim, label, device, ):
     fake_noise = torch.randn(num_images, z_dim, device=device)
     fake_label = F.one_hot(torch.randint(0, 10, (num_images,)), num_classes=label.shape[1])
-    fake = gen(fake_noise,fake_label)
+    fake = gen(fake_noise, fake_label)
     disc_fake_pred = disc(fake)
     gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
     return gen_loss
