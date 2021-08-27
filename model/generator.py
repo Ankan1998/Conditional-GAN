db5448fe5f4c3b5ff4eda4ee0a0796ae5e623f1c
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-27 11:21:57 +0530

    Generator modified

--- model/generator.py
+++ model/generator.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class UpConvBlock(nn.Module):
 
-    def __init__(self,chn_dim, hid_dim,kernel,stride,final_layer=False):
+    def __init__(self,chn_dim, hid_dim=64,kernel=3,stride=2,final_layer=False):
         super(UpConvBlock, self).__init__()
 
         self.final_layer = final_layer
@@ -31,23 +31,26 @@ class Generator(nn.Module):
     def __init__(self, noise_dim, label_dim, hid_dim):
         super(Generator, self).__init__()
         concat_dim = noise_dim + label_dim
-        self.upconv1 = UpConvBlock(concat_dim,hid_dim*2,3,2)
-        self.upconv2 = UpConvBlock(hid_dim*2,hid_dim,3,1)
-        self.upconv3 = UpConvBlock(hid_dim, 3, 3, 1,final_layer=True)
+        self.upconv1 = UpConvBlock(concat_dim,hid_dim*4)
+        self.upconv2 = UpConvBlock(hid_dim*4,hid_dim*2,4,1)
+        self.upconv3 = UpConvBlock(hid_dim * 2, hid_dim)
+        self.upconv4 = UpConvBlock(hid_dim, 1, 4, final_layer=True)
 
     def forward(self,noise,label):
         conc_tensor = torch.cat([noise, label], 1)
         conc_tensor = conc_tensor.view(label.shape[0],conc_tensor.shape[1],1,1)
         gen_inp = self.upconv1(conc_tensor)
         gen_inp = self.upconv2(gen_inp)
-        gen_out = self.upconv3(gen_inp)
+        gen_inp = self.upconv3(gen_inp)
+        gen_out = self.upconv4(gen_inp)
+
 
         return gen_out
 
 
 
 if __name__=="__main__":
-    upcon = Generator(100,10,32)
+    upcon = Generator(100,10,64)
     noise = torch.rand(4,100)
     label = torch.rand(4,10)
     print(upcon)
