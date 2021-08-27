84d09f3bd819270bd57a3aeab5e810c2250febb2
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-27 13:29:40 +0530

    loss corrected with minor change

--- loss.py
+++ loss.py
@@ -7,7 +7,7 @@ def disc_loss(gen, disc, criterion, real, label, num_images, z_dim, device):
     fake_noise = torch.randn(num_images, z_dim, device=device)
     fake_label = F.one_hot(torch.randint(0, 10, (num_images,)), num_classes=label.shape[1])
     fake = gen(fake_noise, fake_label)
-    disc_fake_pred = disc(fake.detach())
+    disc_fake_pred = disc(fake.detach(),fake_label)
     disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
     disc_real_pred = disc(real, label)
     disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
@@ -19,6 +19,6 @@ def gen_loss(gen, disc, criterion, label, num_images, z_dim, device, ):
     fake_noise = torch.randn(num_images, z_dim, device=device)
     fake_label = F.one_hot(torch.randint(0, 10, (num_images,)), num_classes=label.shape[1])
     fake = gen(fake_noise, fake_label)
-    disc_fake_pred = disc(fake)
+    disc_fake_pred = disc(fake,fake_label)
     gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
     return gen_loss
